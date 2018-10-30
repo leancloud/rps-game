d2a337c7407cb7288033012eb9668bca0a68fcb4
leeyeh <lee55962698@gmail.com>
2018-10-30 11:36:24 +0800

    refacrtor: use runtime agnostical LEANCLOUD_APP_ENV variable

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const redisLB = new RedisLoadBalancer(
   gameManager,
   process.env.REDIS_URL_CLIENT_ENGINE_SAMPLE_LB,
   {
-    poolId: `${APP_ID.slice(0, 5)}-${process.env.NODE_ENV || "development"}`,
+    poolId: `${APP_ID.slice(0, 5)}-${process.env.LEANCLOUD_APP_ENV || "development"}`,
   },
 );
 redisLB.on("online", () => console.log("LB redis connected")).on("offline", () => {
