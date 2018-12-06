81c5f52e42b47bf917f8a04eda773155538d1935
leeyeh <lee55962698@gmail.com>
2018-12-06 11:16:43 +0800

    refactor: use LoadBalancerFactory from  @leancloud/client-engine@0.6

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import {
-  Game,
   GameManager,
   ICreateGameOptions,
-  RedisLoadBalancer,
-  RedisLoadBalancerConsumerEvent,
+  LoadBalancerFactory,
 } from "@leancloud/client-engine";
 import bodyParser = require("body-parser");
 import cors = require("cors");
@@ -25,13 +23,7 @@ app.get("/", (req, res) => {
     `);
 });
 
-class SampleGameManager<T extends Game> extends GameManager<T> {
-  public async consume(playerId: string, options: ICreateGameOptions) {
-    return this.makeReservation(playerId, options);
-  }
-}
-
-const gameManager = new SampleGameManager(
+const gameManager = new GameManager(
   PRSGame,
   APP_ID,
   APP_KEY,
@@ -41,21 +33,21 @@ const gameManager = new SampleGameManager(
     // 需要先 import { Region } from "@leancloud/play";
     // region: Region.NorthChina,
   },
-).on(RedisLoadBalancerConsumerEvent.LOAD_CHANGE, () => debug(`Load: ${gameManager.load}`));
-
-const redisLB = new RedisLoadBalancer(
-  gameManager,
-  process.env.REDIS_URL__CLIENT_ENGINE,
-  {
-    poolId: `${APP_ID.slice(0, 5)}-${process.env.LEANCLOUD_APP_ENV || "development"}`,
-  },
 );
-redisLB.on("online", () => console.log("LB redis connected")).on("offline", () => {
-  console.warn(
-`Can not connect to Redis server. Client Engine will keep running in standalone mode.
-It's probably fine if you are running it locally without a Redis server. Otherwise, check project configs.`,
-  );
+
+const loadBalancerFactory = new LoadBalancerFactory({
+  poolId: `${APP_ID.slice(0, 5)}-${process.env.LEANCLOUD_APP_ENV || "development"}`,
+  redisUrl: process.env.REDIS_URL__CLIENT_ENGINE,
 });
+
+const loadBalancer = loadBalancerFactory
+  .bind(gameManager, ["makeReservation"])
+  .on("online", () => console.log("Load balancer online")).on("offline", () => {
+    console.warn(
+`The load balancer can not connect to Redis server. Client Engine will keep running in standalone mode.
+It's probably fine if you are running it locally without a Redis server. Otherwise, check project configs.`,
+    );
+  });
 
 const debug = d("ClientEngine");
 
@@ -73,7 +65,7 @@ app.post("/reservation", async (req, res, next) => {
       throw new Error("Missing playerId");
     }
     debug(`Making reservation for player[${playerId}]`);
-    const roomName = await redisLB.consume(playerId, createGameOptions);
+    const roomName = await gameManager.makeReservation(playerId, createGameOptions);
     debug(`Seat reserved, room: ${roomName}`);
     return res.json({
       roomName,
@@ -97,9 +89,9 @@ app.get("/admin/status", async (req, res, next) => {
   try {
     res.json({
       gameManager: await gameManager.getStatus(),
+      loadBalancer: await loadBalancer.getStatus(),
       memoryUsage: process.memoryUsage(),
       osLoadavg: os.loadavg(),
-      redisLB: await redisLB.getStatus(),
     });
   } catch (error) {
     next(error);
@@ -112,7 +104,7 @@ app.listen(process.env.LEANCLOUD_APP_PORT || 3000);
 process.on("SIGTERM", async () => {
   debug("SIGTERM recieved. Closing the LB.");
   try {
-    await redisLB.close();
+    await loadBalancer.close();
     debug("Shutting down.");
     setTimeout(() => {
       process.exit(0);
