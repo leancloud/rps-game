762637294ea563a0aacc2396d094152cfc03813e
leeyeh <lee55962698@gmail.com>
2018-12-21 15:25:44 +0800

    fix: specify appId/appKey for client
    This is a temporary workaround
    as Client Engine does not inject environment variables at build phase.

--- src/client/configs.ts
+++ src/client/configs.ts
@@ -2,8 +2,8 @@ import { Region } from "@leancloud/play";
 
 export const REGION = Region.NorthChina;
 
-const appId = process.env.LEANCLOUD_APP_ID!;
-const appKey = process.env.LEANCLOUD_APP_KEY!;
+const appId = process.env.LEANCLOUD_APP_ID || "OgE8zDRpOXNwsl3i38vccFqy-9Nh9j0Va";
+const appKey = process.env.LEANCLOUD_APP_KEY || "TMkbH8yfIO2APRioG2joeety";
 if (appId === undefined) {
   throw new Error("LEANCLOUD_APP_ID not set");
 }
