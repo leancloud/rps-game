17e5ddafed35ec88c16cf2e3186819f86942c9cc
Li Ye <liye@xd.com>
2023-12-25 20:16:27 +0800

    chore: migrate to cn-n1

--- src/client/configs.ts
+++ src/client/configs.ts
@@ -1,12 +1,6 @@
-const appId = process.env.LEANCLOUD_APP_ID || "OgE8zDRpOXNwsl3i38vccFqy-9Nh9j0Va";
-const appKey = process.env.LEANCLOUD_APP_KEY || "TMkbH8yfIO2APRioG2joeety";
-const apiServer = process.env.LEANCLOUD_API_SERVER || "https://oge8zdrp.lc-cn-e1-shared.com";
-if (appId === undefined) {
-  throw new Error("LEANCLOUD_APP_ID not set");
-}
-if (appKey === undefined) {
-  throw new Error("LEANCLOUD_APP_KEY not set");
-}
+const appId = "OgE8zDRpOXNwsl3i38vccFqy-9Nh9j0Va";
+const appKey = "TMkbH8yfIO2APRioG2joeety";
+const apiServer = "https://oge8zdrp.lc-cn-n1-shared.com";
 
 const clientEngineServer =
   process.env.NODE_ENV === "development"
